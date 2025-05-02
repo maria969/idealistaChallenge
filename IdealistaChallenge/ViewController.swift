32240eb9bde0859ff1a8628b72020cecbb4d4238
Maria Herrero Varas <maria.herrerovaras@gmail.com>
2025-05-02 23:11:01 +0200

    Initial commit

---
+++ IdealistaChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IdealistaChallenge
+//
+//  Created by Maria Herrero Varas on 2/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
